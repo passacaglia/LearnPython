04f9abeead8faddb5da17fdad2c7ee4d56e10777
Andrew Zhang <snowtown86@gmail.com>
2012-06-17 10:17:52 +0800

    add a tool to search pinyin and uggx

---
+++ MyTools/pinyinuggxSearch.py
@@ -0,0 +1,29 @@
+
+#import wx
+import pyuggx
+#ID_ABOUT = 101
+#ID_EXIT = 110
+
+#class MainWindow(wx.Frame):
+#	def __init__(self,parent,id,title):
+#		wx.Frame.__init__(self,parent,wx.ID_ANY,title,size = (400, 200), style = wx.DEFAULT_FRAME_STYLE | wx.NO_FULL_REPAINT_ON_RESIZE )
+#		self.control = wx.TextCtrl(self,1,style = wx.TE_MULTILINE)
+#		global myval
+#		self.control.SetValue(myval)
+#		self.Show(True)
+		
+#myval = pyuggx.go()
+
+#app = wx.PySimpleApp()
+#frame = MainWindow(None,-1,"Small Editor")
+#frame = wx.Frame(None, -1, "Hello World")
+#frame.Show(1)
+#app.MainLoop()
+
+
+while (True):
+	myval = pyuggx.go()
+	pyuggx.saveToFile(myval)
+	ex = raw_input("\nPress Enter to go on.")
+	
+
---
+++ MyTools/pyuggx.py
@@ -0,0 +1,72 @@
+#encoding:utf-8
+
+import sqlite3
+import time
+import sys
+
+reload(sys)
+print("reload")
+sys.setdefaultencoding('utf-8')
+
+def connect():        
+    global conn, cur
+    conn = sqlite3.connect('pinyinuggx.sqlite')
+    cur = conn.cursor()
+
+
+def query(val):
+	sql = "select * from pinyinuggx where pinyin = '" + val + "' or cc = '" + val + "' order by uggx"
+	cur.execute(sql)
+	
+	res = cur.fetchall()
+	
+	return res
+
+
+def close():
+	conn.close()
+
+	
+def go():
+	hint = '请输入拼音\n > '
+	sea = raw_input(hint)
+	#sea = sea.lower()
+
+	connect()
+
+	res = query(sea)
+	#res 0 1 2 3
+
+	finalstr = "" 
+
+	for r in res:
+		finalstr = finalstr + "\n" + r[2] + "  " + r[1] + "  " + r[3]
+		
+	if ("" == finalstr):
+		finalstr = "Sorry, no '" + sea + "' found!\nWe might not have this in our database.\n"
+	
+	print (finalstr)
+	finalstr = unicode(finalstr)
+	return finalstr
+
+def saveToFile(args):
+    f = file('pyuggxData.txt', 'a')
+    f.write(args)
+    print("write")
+    f.close()
+
+
+	
+	
+# Here we go
+	
+while (True):
+	myval = go()
+	saveToFile(myval)
+	ex = raw_input("\nPress Enter to go on.")
+
+
+
+
+
+	
